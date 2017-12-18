69d5a0f920c262240c17c48de8c5c5a807ea5dc0
Jianxiong Cai <ernestme@icloud.com>
2017-12-19 01:05:50 +0800

    data: add intrinsic matrix

--- src/extract_frames/extract.m
+++ src/extract_frames/extract.m
@@ -1,6 +1,8 @@
 % read in vedio frames
-vedio_path = fullfile('..','..','data','1','Video-2017-12-18-18-59-21_0280.mov');
+vedio_path = fullfile('..','..','data','1','Video-2017-12-18-18-59-21_0280.MOV');
 saving_folder = fullfile('..','..','data','1','rgb');
+% vedio_path = fullfile('..','..','data','test','Video-2017-12-18-20-31-46_0283.MOV');
+% saving_folder = fullfile('..','..','data','test','rgb');
 
 % create saving path
 try
@@ -17,5 +19,6 @@ while (hasFrame(v))
     % save each indivual frames to file system
     file_path = fullfile(pwd,saving_folder,strcat(num2str(frame_id),'.png'));
     imwrite(f,file_path);
+    disp(strcat('[OK] frame_id: ',num2str(frame_id)));
     frame_id = frame_id + 1;
 end
