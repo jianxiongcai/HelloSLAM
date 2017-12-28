c5758002dc062ee724ab53e6fc7ada8d975a03b0
Jianxiong Cai <ernestme@icloud.com>
2017-12-29 02:54:42 +0800

    src: rotate image by 90 degrees

--- frame_extractor/src/extract.m
+++ frame_extractor/src/extract.m
@@ -14,9 +14,10 @@ for k = 1:1:size(vedio_paths,2)
     frame_id = 1;
     while hasFrame(v)
         f = readFrame(v);
+        img =  imrotate(f,-90);
         % save each indivual frames to file system
         file_path = fullfile(pwd,saving_folder,strcat(num2str(frame_id),'.pgm'));
-        imwrite(f,file_path);
+        imwrite(img,file_path);
         disp(strcat('[OK] frame_id: ',num2str(frame_id)));
         frame_id = frame_id + 1;
     end
