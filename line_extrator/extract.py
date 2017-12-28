bc357e0fafdeff3b2c4b76ef2c83a684035f4a7a
Jianxiong Cai <caijx@shanghaitech.edu.cn>
2017-12-29 02:42:40 +0800

    src: working

--- line_extrator/extract.py
+++ line_extrator/extract.py
@@ -1,12 +1,31 @@
 #! /usr/bin/python3
+
+def remove_ext(p):
+	print(p);
+	res = "";
+	for k in range(len(p)):
+		if (p[len(p)-k-1]=="."):
+			break;
+
+	for i in range(len(p)-k-1):
+		res += p[i];
+	return res;
+
+
+
 import os;
-folder_path = "../data";
-folders = os.listdir(folder_path);
-print(folders)
+data_path = "../data";
+folders = os.listdir(data_path);
 for folder in folders:
-	if (os.path.isdir(folder)):
-		for image in os.listdir("../data"):
-			image_path = folder_path+"/"+folder+"/"+image;
-			saving_path = folder_path+"/"+folder+"/"+image;
-			print(image_path);
-			exit(0);
+
+	folder_path = data_path + "/" + folder;
+	print("Checking folder: "+ folder_path)
+
+	if (os.path.isdir(folder_path)):
+		for image in os.listdir(folder_path):
+			image_path = folder_path+"/"+image;
+			saving_path = remove_ext(folder_path+"/"+image);
+			cmd = "./lsd " + " -P " + saving_path +".esp " + image_path + " " +saving_path + ".txt";
+			print(cmd);
+			os.system(cmd);
+			
