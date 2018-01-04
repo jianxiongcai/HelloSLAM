c4ecb4acf04e77c8964524d1280a862852186a0a
Jianxiong Cai <ernestme@icloud.com>
2018-01-05 01:50:17 +0800

    use numbers to extract frames

--- frame_extractor/src/extract.m
+++ frame_extractor/src/extract.m
@@ -1,29 +1,23 @@
 % read in video frames
 % video_paths = {'activity_room_1_video','activity_room_2_video','dorm_room_1_video','dorm_room_2_data','elevator_1_video','elevator_2_video'};
-video_paths = {'180101T162248video','180101T162756video'};
+data_dir = fullfile('..','..','data');
+video_paths = dir(fullfile(data_dir,'*.mp4'));
+video_path = fullfile(data_dir,video_paths(1).name);
+[~,video_name,~] = fileparts(video_path);
 
+saving_folder = fullfile(data_dir,video_name);
+mkdir(saving_folder);
 
-for k = 1:1:size(video_paths,2)
-    video_path = fullfile('..','..','data',strcat(video_paths{k},'.mp4'));
-    saving_folder = fullfile('..','..','data',video_paths{k});
-    mkdir(saving_folder);
-    
-    % extract frames and save to the folder
-    v = VideoReader(video_path);
-    if(k == 1)frame_id = 1514794968524.66;
-    else frame_id = 1514795276996.37;
-    end
-    cnt = 0;
-    while hasFrame(v)
-        f = readFrame(v);
-        % img =  imrotate(f,-90);
-        % save each indivual frames to file system
-        file_path = fullfile(pwd,saving_folder,strcat(num2str(frame_id),'.pgm'));
-        if mod(cnt,3) == 0
-            imwrite(f,file_path);
-            disp(strcat('[OK] frame_id: ',num2str(frame_id)));
-        end
-        frame_id = frame_id + 1/30 * 1000;
-        cnt = cnt + 1;
-    end
+% extract frames and save to the folder
+v = VideoReader(video_path);
+frame_id = 1;
+while hasFrame(v)
+    f = readFrame(v);
+    img =  imrotate(f,-90);
+    % save each indivual frames to file system
+    file_path = fullfile(pwd,saving_folder,strcat(num2str(frame_id),'.pgm'));
+    imwrite(img,file_path);
+    disp(strcat('[OK] frame_id: ',num2str(frame_id)));
+        
+    frame_id = frame_id + 1;
 end
