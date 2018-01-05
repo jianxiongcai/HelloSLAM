62f67de8333c8b92b91032b81221489142c3aa4f
Jianxiong Cai <caijx@shanghaitech.edu.cn>
2018-01-06 01:20:31 +0800

    fix a bug

--- line_extrator/src/merge_lines_img.m
+++ line_extrator/src/merge_lines_img.m
@@ -77,16 +77,3 @@ end
 function [dist] = get_distance(point1, point2)
     dist = norm([point1(1)-point2(1), point1(2) - point2(2)]);
 end
-
-function [lines] = read_in_lines(txt_path)
-% Input:
-%       txt_path:       the full name of the txt path
-% Output:
-%       lines:          4*n matrix (n lines)
-    fid = fopen(fullfile('..','data',txt_path));
-    lines_raw = textscan(fid,'%f %f %f %f %f %f %f');
-    fclose(fid);
-    for i = 1:1:4
-        lines = [lines_raw{1} lines_raw{2} lines_raw{3} lines_raw{4}]';
-    end
-end
--- line_extrator/src/read_in_lines.m
+++ line_extrator/src/read_in_lines.m
@@ -3,7 +3,7 @@ function [lines] = read_in_lines(txt_path)
 %       txt_path:       the full name of the txt path
 % Output:
 %       lines:          4*n matrix (n lines)
-    fid = fopen(fullfile('..','data',txt_path));
+    fid = fopen(txt_path);
     lines_raw = textscan(fid,'%f %f %f %f %f %f %f');
     fclose(fid);
     for i = 1:1:4
