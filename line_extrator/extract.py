abc3274c030dbd6741f2d4846ed22f0f377cc0ce
Jianxiong Cai <caijx@shanghaitech.edu.cn>
2018-01-06 00:44:38 +0800

    extract: save result to lines-raw

--- line_extrator/extract.py
+++ line_extrator/extract.py
@@ -1,5 +1,7 @@
 #! /usr/bin/python3
 
+import os;
+
 def remove_ext(p):
 	print(p);
 	res = "";
@@ -15,17 +17,18 @@ def remove_ext(p):
 
 import os;
 data_path = "../data";
-folders = os.listdir(data_path);
-for folder in folders:
+folder = "rgb";
 
-	folder_path = data_path + "/" + folder;
-	print("Checking folder: "+ folder_path)
+folder_path = data_path + "/" + folder;
+print("Checking folder: "+ folder_path)
 
-	if (os.path.isdir(folder_path)):
-		for image in os.listdir(folder_path):
-			image_path = folder_path+"/"+image;
-			saving_path = remove_ext(folder_path+"/"+image);
-			cmd = "./lsd " + " -P " + saving_path +".esp " + image_path + " " +saving_path + ".txt";
-			print(cmd);
-			os.system(cmd);
-			
+os.mkdir( data_path+"/lines-raw/");
+
+if (os.path.isdir(folder_path)):
+	for image in os.listdir(folder_path):
+		image_path = folder_path+"/"+image;
+		saving_path = remove_ext(data_path+"/lines-raw/"+image);
+		cmd = "./lsd " + " -P " + saving_path +".esp " + image_path + " " +saving_path + ".txt";
+		print(cmd);
+		os.system(cmd);
+		
