e2404c4a1f0ec7d9730497d03ebf374a181dca0d
Xiangchen Zeng <zengxch@shanghaitech.edu.cn>
2018-01-07 22:00:00 +0800

    Optimized by replacing the functions to in-build functions.

--- essential_extractor/src/main.m
+++ essential_extractor/src/main.m
@@ -7,14 +7,12 @@ clc;
 
 % Preprocessing
 load('cameraParams.mat');
-dList = dir('../../data/180101T174834video/*.pgm');
+dList = dir('../../data1/rgb/*.pgm');
 features = cell(2,length(dList));
 validPts = cell(1,length(dList));
 essential = cell(1,length(dList)-1);
 RT = cell(1,length(dList)-1);
 inliers = cell(1,length(dList)-1);
-
-K = cameraParams.IntrinsicMatrix';
 
 % Extract Feature Points
 for i = 1:length(dList)
@@ -31,27 +29,14 @@ for i = 1:length(dList)-1
     indexPairs = matchFeatures(features{2,i},features{2,i+1});
     matchedPoints1 = features{1,i}(indexPairs(:,1));
     matchedPoints2 = features{1,i+1}(indexPairs(:,2));
-    essential{i} = estimateEssentialMatrix(matchedPoints1,matchedPoints2,cameraParams);
-    homoptsA = [matchedPoints1.Location ones(size(matchedPoints1.Location, 1), 1)]';
-    A_temp = K\homoptsA;
-    A_norms = sqrt(sum(A_temp.*A_temp));
-    A_normalized = double(A_temp./repmat(A_norms, 3, 1));
-    homoptsB = [matchedPoints2.Location ones(size(matchedPoints2.Location, 1), 1)]';
-    B_temp = K\homoptsB;
-    B_norms = sqrt(sum(B_temp.*B_temp));
-    B_normalized = double(B_temp./repmat(A_norms, 3, 1));
-    [RT{i}, ilrs] = opengv('fivept_stewenius_ransac', A_normalized, B_normalized);
-    inliers{i} = [homoptsA(:, ilrs); homoptsB(:, ilrs)];
+    [essential{i},inliers] = estimateEssentialMatrix(matchedPoints1,matchedPoints2,cameraParams);
+    inlierPoints1 = matchedPoints1(inliers);
+    inlierPoints2 = matchedPoints2(inliers);
+    [relativeOrientation,relativeLocation] = relativeCameraPose(essential{i},cameraParams,inlierPoints1,inlierPoints2);
+    [rotationMatrix,translationVector] = cameraPoseToExtrinsics(relativeOrientation,relativeLocation);
+    RT{i} = [rotationMatrix,translationVector'];
 end
 
-% Rescale
-for i = 2:length(dList)-1
-    correspondence = find_correspondence(inliers{i}, inliers{i+1});
-    world_point_BA = find_3D_point(correspondence(4:5, :), correspondence(1:2, :), RT{i});
-    world_point_BC = find_3D_point(correspondence(4:5, :), correspondence(7:8, :), RT{i+1});
-    scale = median(world_point_BA(:, 3)./world_point_BC(:, 3));
-    R{i+1}(:,4) = scale * R{i+1}(:,4);
-end
 
 save('RT.mat','RT');
 save('essential.mat','essential');
---
+++ essential_extractor/src/visualization.m
@@ -0,0 +1,11 @@
+clear;
+clc;
+
+load('RT.mat');
+traj = [0;0;0;1];
+for iter= 1:length(RT)
+    traj = [traj [RT{iter};0 0 0 1] * traj(:,iter)];
+end
+
+plot3(traj(1,1),traj(2,1),traj(3,1),'x');
+plot3(traj(1,2:end),traj(2,2:end),traj(3,2:end));
